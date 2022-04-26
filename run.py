72071b6c8abc89ef50329324a911f5523aef2e2f
george-downer40 <george.downer40@gmail.com>
2022-04-26 17:47:34 +0000

    add code in choose_playter function to allow for player to select their starting character

--- run.py
+++ run.py
@@ -19,58 +19,78 @@ class minion:
 
 #functions
 
-def minion_room():
+# def minion_room():
 
 
-def death():
+# def death():
 
 
 def choose_player():
+    player_select = ""
+
+    player_select = input("do you wish to play as\n (1) The Lord?\n (2) The Adventurer?\n (3) The Vagabond?\n")
+    if player_select == "1":
+        print("You have chosen the Lord")
+    elif player_select == "2":
+        print("You have selected the Adventurer")
+    elif player_select == "3":
+        print("You have selected the Vagabond")
+    else:
+        print ("unrecognised input. Please select from the options given")
+        choose_player()
 
 
-def enter_castle():
+
+# def enter_castle():
 
 
-def main_hall():
+# def main_hall():
 
 
-def trap_fail():
+# def trap_fail():
 
 
-def hallway_trap_room():
+# def hallway_trap_room():
 
 
-def curiosity_trap_room():
+# def curiosity_trap_room():
 
 
-def treasure_room():
+# def treasure_room():
 
 
-def bomb_room():
+# def bomb_room():
 
 
-def riddle_room_1():
+# def riddle_room_1():
 
 
-def split_room():
+# def split_room():
 
 
-def trap_room_2():
+# def trap_room_2():
 
 
-def riddle_room_2():
+# def riddle_room_2():
 
 
-def potions_room():
+# def potions_room():
 
 
-def mighty_sword_room():
+# def mighty_sword_room():
 
 
-def boss_fight():
+# def boss_fight():
 
 
-def win_game():
+# def win_game():
 
 
-def play_again():
+# def play_again():
+
+
+
+choose_player()
+choose_player()
+choose_player()
+choose_player()
