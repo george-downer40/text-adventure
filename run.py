b9f49abd2eb3a15c74ab3372dba217e7be865cb9
george-downer40 <george.downer40@gmail.com>
2022-04-26 19:31:27 +0000

    add variables that list player classes and minion classes

--- run.py
+++ run.py
@@ -4,17 +4,33 @@ import time
 #classes
 
 class player:
-    def __init__(self, Phealth, Pattack, Parmour, Pluck):
+
+    def __init__(self, Pname Phealth, Pattack, Parmour, Pluck):
+        self.name = Pname
         self.health = Phealth
         self.attack = Pattack
         self.armour = Parmour
         self.luck = Pluck
 
 class minion:
-    def __init__(self, Mhealth, Mattack, Marmour):
+    def __init__(self, Mname Mhealth, Mattack, Marmour):
+        self.name = Mname
         self.health = Mhealth
         self.attack = Mattack
         self.armour = Marmour
+
+
+player_list = [
+    player("lord", 100, 50, 40, 5),
+    player("Adventurer", 75, 40, 30, 7),
+    player("Vagabond", 50, 20, 20, 12),
+]
+
+minion_list = [
+    minion("Goblin", 25, 20, 10),
+    minion("Skeleton", 25, 20, 10),
+    minion("Kobold", 25, 20, 10),
+]
 
 
 #functions
@@ -26,8 +42,8 @@ class minion:
 
 
 def choose_player():
-    player_select = ""
-
+   # player_select = ""
+    
     player_select = input("do you wish to play as\n (1) The Lord?\n (2) The Adventurer?\n (3) The Vagabond?\n")
     if player_select == "1":
         print("You have chosen the Lord")
@@ -45,7 +61,7 @@ def choose_player():
 
 
 # def main_hall():
-
+ 
 
 # def trap_fail():
 
