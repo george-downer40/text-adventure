19881ca348d7d6b1ce3a98b3f19b422947726a88
george-downer40 <george.downer40@gmail.com>
2022-04-26 16:26:15 +0000

    add expected barebone functions to run.py

--- run.py
+++ run.py
@@ -1,2 +1,61 @@
 import random
 import time
+
+
+#functions
+
+def minion_room():
+
+
+def death():
+
+
+def choose_player():
+
+
+def enter_castle():
+
+
+def main_hall():
+
+
+def trap_fail():
+
+
+def hallway_trap_room():
+
+
+def curiosity_trap_room():
+
+
+def treasure_room():
+
+
+def bomb_room():
+
+
+def riddle_room_1():
+
+
+def split_room():
+
+
+def trap_room_2():
+
+
+def riddle_room_2():
+
+
+def potions_room():
+
+
+def mighty_sword_room():
+
+
+def boss_fight():
+
+
+def win_game():
+
+
+def play_again():
